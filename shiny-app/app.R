c8b0c4e762cd3f69a46fd1108553c7eab217c884
haraee <robbingeo@hotmail.com>
2021-11-10 21:11:05 +0100

    FirstDraft

--- shiny-app/app.R
+++ shiny-app/app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(tidyverse)
 library(ggplot2)
+library(dplyr)
 library(nycflights13)
 
 # Loading all data
@@ -10,19 +11,50 @@ flights <- nycflights13::flights
 planes <- nycflights13::planes
 weather <- nycflights13::weather
 
+variables <- colnames(flights)
 
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Visualizing Flights Data: package nycflights13"),
-
-    ### UI LOGIC
+    titlePanel("Visualizing Flights Data: package nycflights13"),  
+     sidebarLayout(
+         sidebarPanel(
+            selectInput("selectedVariable", "For which variable would you like to display summary statistics?" , choices = variables),
+            selectInput("Filter1", "Filtered on?" , choices = variables),
+            sliderInput("Slider1", "Which values" , min = as.integer(output$sometest), max = 1000),
+         ), 
+         mainPanel (
+            tableOutput("FirstTable"), 
+            plotOutput("Histogram") 
+         )
+     )
 )
-
 
 server <- function(input, output) {
     
-    ### SERVER LOGIC
+    data <- reactive(
+        flights
+    )
+    
+    filteredData <- reactive(
+        filter(data(), carrier == "UA")
+    )
+    
+    output$sometest <- renderText({
+        min(data()[[input$Filter1]] , na.rm = TRUE)
+    })
+    
+    output$FirstTable <- renderTable(
+       head(data())
+    )
+    
+    output$Histogram <- renderPlot({
+        
+        hist(filteredData()[[input$selectedVariable]] , 
+             main = paste("Histogram of", toString(input$selectedVariable)),
+             xlab = toString(input$selectedVariable),
+             col="blue",
+             )
+        }
+    )
     
 }
 
