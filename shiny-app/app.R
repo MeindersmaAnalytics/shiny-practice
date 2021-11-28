cf413e75706ad70c5de53ec81403a234799b028f
MeindersmaAnalytics <info@meindersma-analytics.com>
2021-11-28 18:38:51 +0100

    Improved the lay-out, order of reactive expressions, destination is no longer removed after updating delay type etc.

--- shiny-app/app.R
+++ shiny-app/app.R
@@ -24,7 +24,9 @@ flights <- flights %>%
            dest = as.factor(dest)) %>% 
     pivot_longer(cols = c("dep_delay", "arr_delay"),
                  names_to = "delay_type", 
-                 values_to = "delay_time")
+                 values_to = "delay_time") %>% 
+    mutate(delay_type = ifelse(delay_type %in% "dep_delay",
+                               "Departure", "Arrival"))
 
 # Order level of factors alphabetically
 levels(flights$origin) <- rev(unique(rev(flights$origin)))
@@ -35,21 +37,38 @@ ui <- fluidPage(
     # Application title
     titlePanel("New York Flights"),
     
-    # select origin airport
-    selectInput("origin", "Origin airport:", choices = levels(flights$origin)),
-
-    # select destination airport
-    selectInput("dest", "Destination airport:", choices = NULL),
-    
-    # select date of the flight
-    dateRangeInput("date", "Flight date:", 
-                   min = min(flights$time_hour), 
-                   max = max(flights$time_hour), 
-                   start = min(flights$time_hour),
-                   end = max(flights$time_hour)),
-    
-    # visualizing departure and arrival delay
-    plotOutput("delay_plot")
+    sidebarLayout(
+        sidebarPanel(
+            # select origin airport
+            selectInput("origin", "Origin airport:", choices = levels(flights$origin)),
+            
+            # select destination airport
+            selectInput("dest", "Destination airport:", choices = NULL, multiple = TRUE),
+            
+            # select delay type: departure or arrival
+            selectInput("delay_type", "Delay type:", choices = c("Departure", "Arrival")),
+            
+            # select date of the flight to filter on
+            dateRangeInput("date", "Flight date:", 
+                           min = min(flights$time_hour), 
+                           max = max(flights$time_hour), 
+                           start = min(flights$time_hour),
+                           end = max(flights$time_hour)),
+            
+            # select carriers to filter on
+            # selectInput("carrier", "Carrier:", choices = NULL, multiple = TRUE),
+            
+            # select planes to filter on
+            # selectInput("plane", "Plane:", choices = NULL, multiple = TRUE),
+            
+            width = 3
+        ),
+        mainPanel(
+            # visualizing departure and arrival delay
+            plotOutput("delay_plot"), 
+            width = 9
+        )
+    )
 )
 
 
@@ -57,29 +76,41 @@ server <- function(input, output) {
     
     # reactive expression for filtering based on origin
     select_origin <- reactive({
-        flights %>% filter(origin == input$origin)
+        flights %>% filter(origin == input$origin) %>% 
+            group_by(dest) %>% 
+            filter(n() >= 5)
     })
     
+    # update the destination choices based on the origin choice
     observeEvent(select_origin(), {
-        choices <- levels(droplevels(select_origin()$dest))
+        choices <- sort(levels(droplevels(select_origin()$dest)))
         updateSelectInput(inputId = "dest", choices = choices) 
     })
     
-    # reactive expression for filtering the data
-    selected <- reactive(select_origin() %>% filter(dest == input$dest,
-                                              time_hour %within% interval(input$date[1], input$date[2])))
+    # reactive expression for filtering based on origin
+    select_delay_type <- reactive({
+        flights %>% filter(delay_type == input$delay_type,
+                           !is.na(delay_type))
+    })
     
-    output$delay_plot <- renderPlot(
+    # reactive expression for filtering the data
+    selected <- reactive(select_delay_type() %>% filter(dest == input$dest,
+                                                        time_hour %within% interval(input$date[1], input$date[2])))
+    
+    output$delay_plot <- renderPlot({
+        # If no selections are made for destination, show this message:
+        validate(need(input$dest, 'Please select at least one destination airport!'))
         selected() %>% 
-            ggplot(aes(x = delay_time, group = delay_type, fill = delay_type)) +
-            geom_density(adjust=2, alpha=0.4) +
+            ggplot(aes(x = delay_time, group = dest, fill = dest)) +
+            geom_density(adjust=1, alpha=0.2) +
             ylab("Density") +
-            labs(title = "Origin to Destination: Departure and Arrival Delay in minutes") +
+            labs(title = paste0(input$delay_type, " Delay Distribution: Origin to Destination")) +
             scale_x_continuous(name = "Delay (in minutes)", 
-                               breaks = seq(-120, 240, 30),
-                               limits = c(-120, 240),
-                               labels = seq(-120, 240, 30),
+                               breaks = seq(-60, 120, 30),
+                               limits = c(-60, 120),
+                               labels = seq(-60, 120, 30),
                                expand = expansion(mult = c(0, 0)))
+    }
     )
 }
 
